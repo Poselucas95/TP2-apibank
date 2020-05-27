786ed79150c6cf4ad01a9a01e4be3f1200c25cc5
lpose <lucas.pose@mercadolibre.com>
2020-05-27 12:40:06 -0300

    Inicio

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Bienvenido a la aplicación del banco.");
+});
+
+
+
+
+
+
+const server = app.listen(port, (error) => {
+  if (error) {
+    return console.log(`Error: ${error}`);
+  }
+  console.log(`Server listening on port ${server.address().port}`);
+});
