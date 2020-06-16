ed018c8d8e0bde5c923cbb601a16105f11a0f363
Julian Luco Contestin <julianluco@MacBook-Pro-de-Julian.local>
2020-06-15 21:16:00 -0300

    metodos de cuentas

--- data/cuentas.js
+++ data/cuentas.js
@@ -1,24 +1,132 @@
 const db = require("./conecction");
+const validator = require("validator").default;
+const chalk = require("chalk");
+
+async function getAccounts(dni) {
+  var clientmongo = await db.getConnection();
+  var client = await clientmongo
+    .db("apibank")
+    .collection("clientes")
+    .find({ person_id: parseInt(dni) })
+    .toArray();
+
+  if (client && client.length !== 1) {
+    return 1;
+  }
+  if (client[0].accounts) {
+    var accountsArray = [];
+    for (const element of client[0].accounts) {
+      var acc = await clientmongo
+        .db("apibank")
+        .collection("cuentas")
+        .find({ account_id: element })
+        .toArray();
+      c;
+      if (acc && acc.length === 1) {
+        accountsArray.push(acc[0]);
+      }
+    }
+
+    return accountsArray;
+  }
+}
+
+async function newAccount(dni, values) {
+  var clientmongo = await db.getConnection();
+  var client = await clientmongo
+    .db("apibank")
+    .collection("clientes")
+    .find({ person_id: parseInt(dni) })
+    .toArray();
+  if (client && client.length !== 1) {
+    return 1;
+  }
+  if (
+    (values.type === "CA" || values.type === "CC") &&
+    validator.isLength(values.alias, { min: 5, max: 15 })
+  ) {
+    var auxAccount = await clientmongo
+      .db("apibank")
+      .collection("cuentas")
+      .find({ alias: values.alias })
+      .toArray();
+    if (auxAccount && auxAccount.length > 0) {
+      return 2;
+    }
+    var lastAccount = await clientmongo
+      .db("apibank")
+      .collection("cuentas")
+      .find()
+      .sort({ $natural: -1 })
+      .limit(1)
+      .toArray();
+
+    await clientmongo
+      .db("apibank")
+      .collection("cuentas")
+      .insertOne({
+        account_id: lastAccount[0].account_id + 1,
+        alias: values.alias,
+        type: values.type,
+        limit: values.type === "CA" ? 0 : client[0].salary * 0.1,
+        balance: 0,
+      })
+      .then((res) => {
+        console.log(chalk.green(`Se insertó ${res.insertedCount} cuenta`));
+      })
+      .catch((err) => {
+        console.log(chalk.red(err));
+      });
+
+    await clientmongo
+      .db("apibank")
+      .collection("clientes")
+      .updateOne(
+        { person_id: parseInt(dni) },
+        { $set: { accounts: client[0].accounts.concat(lastAccount[0].account_id + 1) } }
+      );
+
+    await clientmongo
+      .db("apibank")
+      .collection("transacciones")
+      .insertOne({ 
+        account_id: lastAccount[0].account_id + 1,
+        transaction_count: 0,
+        transactions: []
+      });
+
+      return 0
+  }
+}
 
 
-
-async function getAccounts(id) {
+async function updateAccount(acc, values) {
   var clientmongo = await db.getConnection();
   var account = await clientmongo
     .db("apibank")
     .collection("cuentas")
-    .find({ account_id: id })
+    .find({ account_id: parseInt(acc) })
     .toArray();
-  return account;
+
+  if (account && account.length !== 1) {
+    return 0;
+  }
+
+  if(validator.isLength(values.alias, { min: 5, max: 15 })){
+    var searchAliasAccount = await clientmongo.db("apibank").collection("cuentas").find({alias: values.alias}).toArray();
+    if(searchAliasAccount && searchAliasAccount.length > 0){
+      return 0
+    }
+    await clientmongo.db("apibank").collection("cuentas").updateOne({ account_id: account[0].account_id }, { $set: { alias: values.alias }}).then(res => {
+      console.log(chalk.green(`Se modificó ${res.modifiedCount} registro`));
+      return 2;
+    }).catch(err => {
+      chalk.red("No se logro editar la cuenta", err);
+    })
+  }
+
+  return 1;
+
 }
 
-module.exports = { getAccounts };
-
-/* 
-var collection = await clientmongo
-.db("apibank")
-.collection("cuentas")
-.find()
-.sort({ $natural: -1 })
-.limit(1)
-.toArray(); */
+module.exports = { getAccounts, newAccount, updateAccount };
--- routes/cuentas.js
+++ routes/cuentas.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var dataCuentas = require('../data/cuentas')
+var express = require("express");
 var router = express.Router();
+var dataCuentas = require("../data/cuentas");
 
-/* GET cuentas listing. */
-router.get('/', function(req, res, next) {
-  res.send('cuentas, respond with a resource');
+// GET cuentas
+router.get("/:dni", async function (req, res, next) {
+    var accounts = await dataCuentas.getAccounts(req.params.dni);
+    if(accounts === 1){
+      res.send('Client not found', 404)
+    }
+    if (accounts && accounts.length > 0) {
+      res.send(accounts, 200);
+    } else {
+      res.send("Accounts not found", 404);
+    }
 });
 
-router.get(`/getAccount/:acc`, async function(req, res, next) {
+// POST cuentas
+router.post("/:dni", async function (req, res, next){
+    var newAccount = await dataCuentas.newAccount(req.params.dni, req.body)
+    if(newAccount === 1){
+      res.send("Client not found", 404)
+    }
+    if(newAccount === 2){
+      res.send("Error: params fail", 400)
+    }
+    if(newAccount === 0){
+      res.send("Account create", 200)
+    }
+    res.send("Error", 400)
+})
 
-    var account = await dataCuentas.getAccount(req.params.acc)
-    if(account && account != {}){
-      res.send(account, 200)
-    }else {
-      res.send("Account not found", 404)
+// Update cuenta
+router.put("/:account", async function (req, res, next){
+    var updateAccount = await dataCuentas.updateAccount(req.params.account, req.body)
+    if(updateAccount === 0){
+      res.send("account not found", 404)
+    }
+    if(updateAccount === 1){
+      res.send("Error", 400)
+    }
+    if(updateAccount === 2 ){
+      res.send("Update complete", 200)
     }
 })
+
 
 module.exports = router;
