ea9ce59d3e0cba97c05430b9ae6710e60d928975
lpose <lucas.pose@mercadolibre.com>
2020-06-17 20:25:33 -0300

    Creacion de credito

--- routes/creditos.js
+++ routes/creditos.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var dataCreditos = require("../data/creditos");
 
-/* GET creditos listing. */
-router.get('/', function(req, res, next) {
-  res.send('creditos, respond with a resource');
-});
+
+// POST Credito
+router.post("/:alias", async function (req, res, next){
+  var newCredit = await dataCreditos.newCredit(req.params.alias, req.body)
+  if(newCredit === 1){
+    res.send("Account not found", 404)
+  }
+  if(newAccount === 2){
+    res.send("The reason is null", 400)
+  }
+  if(newAccount === 3){
+    res.send("The ammout must be above 0", 400)
+  }
+  if(newAccount === 4){
+    res.send("Transaction added correctly", 200)
+  }
+})
+
+
 
 module.exports = router;
