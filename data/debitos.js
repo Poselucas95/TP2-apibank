9fa6bd08a957276bb64dd837c0e865e095a791f8
Julian Luco Contestin <julianluco@MacBook-Pro-de-Julian.local>
2020-06-18 12:53:38 -0300

    debitos

---
+++ data/debitos.js
@@ -0,0 +1,74 @@
+const db = require("./conecction");
+const _ = require("lodash");
+const validator = require("validator").default;
+const chalk = require("chalk");
+
+async function newDebit(alias, values) {
+  if (
+    !values.reason ||
+    validator.isEmpty(values.reason) ||
+    values.reason.length < 2
+  ) {
+    return 2;
+  }
+  if (!values.amount || values.amount < 0) {
+    return 3;
+  }
+
+  var account = await clientmongo
+    .db("apibank")
+    .collection("cuentas")
+    .find({ alias: alias })
+    .toArray();
+
+  if (account && account.length !== 1) {
+    return 1;
+  }
+
+  if(account[0] && account[0].balance + account[0].limit - values.amount < 0){
+      return 4
+  }
+
+  var objectToPost = {
+    date: new Date(),
+    origin: "api",
+    alias: alias,
+    type: "debito",
+    amount: values.amount,
+    reason: values.reason,
+  };
+
+  var accountTransaction = await clientmongo
+    .db("apibank")
+    .collection("transacciones")
+    .find({ account_id: account[0].account_id });
+
+  await clientmongo
+    .db("apibank")
+    .collection("transacciones")
+    .updateOne(
+      { account_id: parseInt(account[0].account_id) },
+      {
+        $set: {
+          transactions: accountTransaction[0].transactions.concat(objectToPost),
+          transaction_count: accountTransaction[0].transaction_count + 1,
+        },
+      }
+    );
+
+  await clientmongo
+    .db("apibank")
+    .collection("cuentas")
+    .updateOne(
+      { account_id: parseInt(account[0].account_id) },
+      {
+        $set: {
+          balance: account[0].balance - values.amount,
+        },
+      }
+    );
+
+  return 5;
+}
+
+module.exports = { newDebit };
--- routes/debitos.js
+++ routes/debitos.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var dataDebitos = require("../data/debitos");
 
-/* GET debitos listing. */
-router.get('/', function(req, res, next) {
-  res.send('debitos, respond with a resource');
-});
+
+// POST Debito
+router.post("/:alias", async function (req, res, next){
+  var newDebit = await dataDebitos.newDebit(req.params.alias, req.body)
+  if(newDebit === 1){
+    res.send("Account not found", 404)
+  }
+  if(newDebit === 2){
+    res.send("The reason is null", 400)
+  }
+  if(newDebit === 3){
+    res.send("The ammout must be above 0", 400)
+  }
+  if(newDebit === 4){
+    res.send("The account's limit isn't enough", 400)
+  }
+  if(newDebit === 5){
+    res.send("Transaction added correctly", 200)
+  }
+})
+
+
 
 module.exports = router;
